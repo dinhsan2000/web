707ce02ab423f13a38d156452db54d7e3a8348a6
dinhsan2000 <dinhsan200@gmail.com>
2021-07-30 00:54:12 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMENU = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close'),
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -7,7 +9,11 @@
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMENU.classList.add('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
